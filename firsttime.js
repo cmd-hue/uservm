3d2dd45e4fe55f46de265e980e177c399f25ea04
giggityfiles <giggityfiles@gmail.com>
2026-02-17 16:25:03 -0500

    changes

--- firsttime.js
+++ firsttime.js
@@ -1,16 +1,16 @@
 function firstTimeInit() {
-    openModal('welcome-modal');
+    return; // openModal('welcome-modal');
 }
 
 function firstTimeSetupLoadDefaults() {
-    $('#welcome-setup-username').val('cmdhue-cvm' + (Math.floor(Math.random() * 8999) + 1000).toString());
-    $('#welcome-setup-server-list').val(``);
+    $('#welcome-setup-username').val('cvm-hue-user' + (Math.floor(Math.random() * 8999) + 1000).toString());
+    $('#welcome-setup-server-list').val(`vps1.shijimanet.com`);
 }
 
 function firstTimeSetupSaveSettings() {
     localStorage.setItem('username', $('#welcome-setup-username').val());
     localStorage.setItem('servers', JSON.stringify($('#welcome-setup-server-list').val().split('\n')));
-    localStorage.setItem('chatSound', 'http://computernewb.com/collab-vm/notify.ogg')
+    localStorage.setItem('chatSound', 'https://file.garden/aUYIWVAKvQxCBY-_/sfx/Click.mp3')
     localStorage.setItem('showChatTime', JSON.stringify(true));
     localStorage.setItem('showChatTimeInUTC', JSON.stringify(true));
     localStorage.setItem('vmViewFit', JSON.stringify(false));
