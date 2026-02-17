c198b07829fc38391f43d06ec38552ce0cc1ea64
giggityfiles <giggityfiles@gmail.com>
2026-02-17 16:06:55 -0500

    fixed + recorder

--- index.js
+++ index.js
@@ -536,12 +536,56 @@ loadVMList();
 
 $('#loading').hide();
 
-var screenshotvm = (function() {
-    var c = document.querySelector("canvas");
-    if (!c) {
+var screenshotvm = (function () {
+
+    function run() {
+        var c = document.querySelector("canvas");
+        if (!c) {
+            requestAnimationFrame(run);
+            return;
+        }
+
+        var link = document.createElement("a");
+        link.download = "vm.png";
+        link.href = c.toDataURL("image/png");
+        link.click();
     }
-    var link = document.createElement("a");
-    link.download = "vm.png";
-    link.href = c.toDataURL("image/png");
-    link.click();
+
+    if (document.readyState === "complete") {
+        run();
+    } else {
+        window.addEventListener("load", run);
+    }
+
 })();
+var videovm = (function () {
+
+    var recorder = null;
+    var chunks = [];
+    var recording = false;
+
+    return function () {
+
+        var canvas = document.querySelector("canvas");
+        if (!canvas) return;
+
+        if (!recording) {
+            // START RECORDING
+            var stream = canvas.captureStream(60); // 60 FPS
+            recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+            chunks = [];
+
+            recorder.ondataavailable = function (e) {
+                if (e.data.size > 0) chunks.push(e.data);
+            };
+
+            recorder.onstop = function () {
+                var blob = new Blob(chunks, { type: "video/webm" });
+                var url = URL.createObjectURL(blob);
+
+                var a = document.createElement("a");
+                a.href = url;
+                a.download = "vm.webm";
+                a.click();
+
+                URL.revok
