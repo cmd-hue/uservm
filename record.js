e7753e02295365976766995557a38d28bdb6d78a
giggityfiles <giggityfiles@gmail.com>
2026-02-18 15:00:25 -0500

    d

---
+++ record.js
@@ -0,0 +1,77 @@
+var recbutton = document.getElementsByClassName("videorec")[0]
+var videovm = (function() {
+
+    var recorder = null;
+    var chunks = [];
+    var recording = false;
+    var rafId = null;
+
+    function forceFrames(canvas) {
+        var ctx = canvas.getContext("2d");
+
+        function tick() {
+            ctx.globalAlpha = 0.00001;
+            ctx.fillRect(0, 0, 1, 1);
+            ctx.globalAlpha = 1;
+
+            rafId = requestAnimationFrame(tick);
+        }
+
+        tick();
+    }
+
+    return function () {
+
+        var canvas = document.querySelector("canvas");
+        if (!canvas) return;
+
+        if (!recording) {
+
+            var stream = canvas.captureStream(60);
+            recorder = new MediaRecorder(stream, {
+                mimeType: "video/webm;codecs=vp9"
+            });
+
+            chunks = [];
+
+            recorder.ondataavailable = function (e) {
+                if (e.data.size > 0) chunks.push(e.data);
+            };
+
+            recorder.onstop = function () {
+
+                cancelAnimationFrame(rafId);
+
+                var blob = new Blob(chunks, { type: "video/webm" });
+                var url = URL.createObjectURL(blob);
+
+                var a = document.createElement("a");
+                a.href = url;
+                a.download = "vm.webm";
+                a.click();
+
+                URL.revokeObjectURL(url);
+            };
+
+            forceFrames(canvas);
+            recorder.start();
+
+            if (typeof recbutton !== "undefined") {
+                recbutton.innerHTML = "Recording...";
+            }
+
+            recording = true;
+
+        } else {
+
+            recorder.stop();
+
+            if (typeof recbutton !== "undefined") {
+                recbutton.innerHTML = "Record VM Screen";
+            }
+
+            recording = false;
+        }
+    };
+
+})();
