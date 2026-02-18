18e7c2535f81d4cfd8a53bdd1b5e0f61e09888a7
giggityfiles <giggityfiles@gmail.com>
2026-02-17 20:26:51 -0500

    fix

--- index.js
+++ index.js
@@ -559,7 +559,7 @@ var screenshotvm = function() {
     }
 
 };
-var recbutton = document.getElementsByClassName("videorec")
+var recbutton = document.getElementsByClassName("videorec")[0]
 var videovm = (function () {
 
     var recorder = null;
