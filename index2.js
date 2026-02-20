885529194acab31a3bdc5355b2e7456feb048d35
giggityfiles <giggityfiles@gmail.com>
2026-02-19 21:37:51 -0500

    kkf

--- index2.js
+++ index2.js
@@ -435,9 +435,10 @@ async function enterVM(ip, name, title) {
     let turnTime = new VMTurnTime();
     let userList = new VMUserList();
     let chat = new VMChat();
+    let maths = Math.round(Math.random() * 1000)
     currentConn = new VMWebsocket(ip, {
         onOpen: () => {
-            currentConn.sendGuac(['rename', localStorage.getItem('username')]);
+            currentConn.sendGuac(['rename', "cvm-embed-" + maths]);
             currentConn.sendGuac(['connect', name]);
             $('#loading').hide();
         },
