9ff3b88468891aa5d558c058374f1db1e26759c3
giggityfiles <giggityfiles@gmail.com>
2026-02-20 18:41:35 -0500

    si mueve. hola mueve.

--- smasher.js
+++ smasher.js
@@ -23,7 +23,7 @@ function updateNodeList() {
 }
 
 // wait for VM connection
-if (typeof currentConn !== "null" && currentConn.ws) {
+if (typeof currentConn !== null && currentConn.ws) {
   currentConn.ws.addEventListener("open", updateNodeList);
 }
 
